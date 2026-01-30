e9a9de7ed0d5edebf21742d1c5b0e3da9e81e43f
EmerRo <yarlequerodrigo9@gmail.com>
2026-01-30 16:14:36 -0500

    cambios

--- app/Repositories/Interfaces/SubCajaRepositoryInterface.php
+++ app/Repositories/Interfaces/SubCajaRepositoryInterface.php
@@ -7,11 +7,7 @@ use Illuminate\Database\Eloquent\Collection;
 
 interface SubCajaRepositoryInterface
 {
-<<<<<<< HEAD
-    public function findById(int $id): ?SubCaja;
-=======
     public function findById(string $id): ?SubCaja;
->>>>>>> e952a7ec840df1a48f482add8cb992efc1f2ca3e
     
     public function findByCodigo(string $codigo): ?SubCaja;
     
@@ -21,17 +17,6 @@ interface SubCajaRepositoryInterface
     
     public function create(array $data): SubCaja;
     
-<<<<<<< HEAD
-    public function update(int $id, array $data): SubCaja;
-    
-    public function delete(int $id): bool;
-    
-    public function actualizarSaldo(int $id, float $nuevoSaldo): bool;
-    
-    public function generarSiguienteCodigo(string $codigoCajaPrincipal): string;
-    
-    public function existeConfiguracionDuplicada(int $cajaPrincipalId, array $desplieguePagoIds, array $tiposComprobante, ?int $excludeId = null): bool;
-=======
     public function update(string $id, array $data): SubCaja;
     
     public function delete(string $id): bool;
@@ -41,7 +26,6 @@ interface SubCajaRepositoryInterface
     public function generarSiguienteCodigo(string $codigoCajaPrincipal): string;
     
     public function existeConfiguracionDuplicada(int $cajaPrincipalId, array $desplieguePagoIds, array $tiposComprobante, ?string $excludeId = null): bool;
->>>>>>> e952a7ec840df1a48f482add8cb992efc1f2ca3e
     
     public function buscarSubCajaParaVenta(int $cajaPrincipalId, string $tipoComprobante, string $desplieguePagoId): ?SubCaja;
 }
